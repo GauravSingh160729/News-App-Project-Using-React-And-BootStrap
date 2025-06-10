d5fb909c4b4bb5cab5a2efeda6ca17c2073b8be3
GauravSingh160729 <gauravsingh160729@gmail.com>
2025-06-10 19:03:50 +0530

    News Website Project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import Navbar from './components/Navbar'
+import './App.css'
+import NewsBoard from './components/NewsBoard'
+import { useState } from 'react'
+useState
+function App() {
+  return (
+    <>
+      <Navbar />
+      <NewsBoard />
+    </>
+  )
+}
+
+export default App
